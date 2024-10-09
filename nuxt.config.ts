e2d70ba4fdc97324df8f2f7f05ba43cc2f880bfc
Filippo Locoro <filippo.locoro@camarasoft.com>
2024-10-09 22:44:38 +0200

    feat: add nuxt hub deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxthub/core'],
+  hub: {
+    database: true,
+    kv: true,
+    blob: false, // Please enable R2 Storage on your Cloudflare account to leverage Blob storage and try again: https://dash.cloudflare.com/37d02e01a9ccea9dd863b8e8bc7485c0/r2/plans
+    cache: false,
+  },
 })
