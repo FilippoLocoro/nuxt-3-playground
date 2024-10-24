f71e6f2a0303eca35884c34c575269b60d60acb6
Filippo Locoro <filippo.locoro@camarasoft.com>
2024-10-25 00:05:30 +0200

    feat: improve eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  root: true,
+  root: true, // This ensures that the current directory is treated as the root directory for ESLint,
   env: {
-    browser: true,
-    node: true,
-    es2021: true,
+    browser: true, // Specifies that global browser variables (e.g., window, document) are allowed.
+    node: true, //  Specifies that global Node.js variables and Node.js scoping are allowed.
+    es2021: true, // Specifies that ECMAScript 2021 globals and syntax (like optional chaining, nullish coalescing) are allowed.
   },
   parserOptions: {
-    ecmaVersion: 2021,
-    parser: '@typescript-eslint/parser', // TypeScript parser
-    sourceType: 'module',
+    ecmaVersion: 2021, // Sets the ECMAScript syntax version to 2021, enabling modern JavaScript features.
+    parser: '@typescript-eslint/parser', // This parser allows ESLint to understand TypeScript syntax, making sure that ESLint can lint .ts and .vue files with TypeScript code.
+    sourceType: 'module', // ndicates that ECMAScript modules (ESM) are used, which is typical for modern JavaScript projects like Nuxt.
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-recommended', // Vue 3 linting rules
-    'plugin:@typescript-eslint/recommended', // TypeScript linting rules
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays Prettier errors as ESLint errors
+    'plugin:vue/vue3-recommended', // Lints Vue 3 files properly.
+    'plugin:@typescript-eslint/recommended', // Provides recommended rules for TypeScript linting.
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays Prettier errors as ESLint errors - Ensures Prettier formatting is checked as part of ESLint.
   ],
   rules: {
     // Add your custom rules here or override specific rules
