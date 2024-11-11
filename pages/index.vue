7f5c8ffe439daa660ebd47807cddd7da0439d0ff
Filippo Locoro <filippo.locoro@camarasoft.com>
2024-11-11 22:14:17 +0100

    feat: better query management, improve code

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
     <h1>{{ page.title }}</h1>
-    <figure class="image">
+    <figure v-if="page?.featuredImage?.responsiveImage" class="image">
       <datocms-image :data="page.featuredImage.responsiveImage" />
     </figure>
     <datocms-structured-text :data="page.content" />
 
-    <div v-for="category in categories" :key="category" class="card sm:card-side max-w-sm sm:max-w-full">
+    <div v-for="category in categories" :key="category.id" class="card sm:card-side max-w-sm sm:max-w-full">
       <figure v-if="category?.featuredImage?.responsiveImage">
         <datocms-image :data="category.featuredImage.responsiveImage" />
       </figure>
       <div class="card-body">
         <h5 class="card-title mb-2.5">{{ category.title }}</h5>
-        <!-- <p class="mb-3">Esplora tutte le possibilità e le novità che la Scuola Sci e Snowboard Sandomenico offre</p> -->
         <div class="card-actions">
           <NuxtLinkLocale :to="`/category/${category.link.slug}`">{{ category.link.slug }}</NuxtLinkLocale>
         </div>
@@ -22,96 +21,39 @@
 </template>
 
 <script setup lang="ts">
-import { toHead, Image as DatocmsImage, StructuredText as DatocmsStructuredText } from 'vue-datocms';
+import { pageQuery, allCategoriesQuery } from '~/utils/graphql'
+import { Image as DatocmsImage, StructuredText as DatocmsStructuredText } from 'vue-datocms';
 
-// import { imageFields, seoMetaTagsFields, formatDate } from '~/utils/graphql'
+// Internationalization setup
+const { locale } = useI18n();
 
-// import { activityQuery } from '~/graphql/queries/activity'
+// Variables and GraphQL queries
+const slug = ref('home');
 
-const { locale, locales } = useI18n()
-const route = useRoute()
+// State variables for page and categories data
+const page = ref({});
+const categories = ref([]);
 
-const slug = 'home'
+// Fetch data concurrently and handle possible null values
+async function fetchData() {
+  try {
+    const [pageData, categoriesData] = await Promise.all([
+      useGraphqlQuery({
+        query: pageQuery,
+        variables: { slug: slug.value, locale: locale.value }
+      }),
+      useGraphqlQuery({
+        query: allCategoriesQuery,
+        variables: { locale: locale.value }
+      })
+    ]);
 
-const [pageData, categoriesData] = await Promise.all([
-  useGraphqlQuery({
-    query: `
-      query Page($slug: String!) {
-        page(locale: ${locale.value}, filter: {slug: {eq: $slug}}) {
-          slug
-          title
-          id
-          featuredImage {
-            height
-            url
-            width
-            title
-            format
-            size
-            alt
-            responsiveImage {
-              aspectRatio
-              alt
-              base64
-              bgColor
-              height
-              sizes
-              src
-              srcSet
-              title
-              webpSrcSet
-              width
-            }
-          }
-          content {
-            blocks
-            links
-            value
-          }
-        }
-      }
-    `,
-    variables: { slug, locale: locale.value }
-  }),
-  useGraphqlQuery({
-    query: `
-      query AllCategories {
-        allCategories {
-          id
-          title
-          featuredImage {
-            responsiveImage {
-              aspectRatio
-              alt
-              base64
-              bgColor
-              height
-              sizes
-              src
-              srcSet
-              title
-              webpSrcSet
-              width
-            }
-            height
-            url
-            width
-          }
-          link {
-            slug(locale: it)
-            title(locale: it)
-          }
-        }
-      }
-    `
-  })
-]);
+    page.value = pageData?.data?.value?.page || {};
+    categories.value = categoriesData?.data?.value?.allCategories || [];
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
-
-const page = pageData.data.value.page || {}
-const categories = categoriesData.data.value.allCategories || [];
-// const categories = computed(() => categoriesData.value?.allCategories || [])
-
-// useHead(() => toHead(data.value?.site?.favicon || {}))
-
+fetchData();
 </script>
--- utils/graphql.ts
+++ utils/graphql.ts
@@ -1,31 +1,53 @@
 import { format, parseISO } from 'date-fns';
 
-export const responsiveImageFields = `
-  fragment responsiveImageFields on ResponsiveImage {
-    aspectRatio
-    alt
-    base64
-    bgColor
-    height
-    sizes
-    src
-    srcSet
-    title
-    webpSrcSet
-    width
+// export const responsiveImageFields = `
+//   fragment responsiveImageFields on ResponsiveImage {
+//     aspectRatio
+//     alt
+//     base64
+//     bgColor
+//     height
+//     sizes
+//     src
+//     srcSet
+//     title
+//     webpSrcSet
+//     width
+//   }
+// `;
+
+export const contentFieldFragment = `
+  fragment contentFieldFragment on PageModelContentField {
+    blocks
+    links
+    value
   }
 `;
 
-// export const imageFields = `
-//   fragment imageFields on ImageBlockRecord {
-//     responsiveImage {
-//       ...responsiveImageFields
-//     }
-//     alt
-//     height
-//     url
-//   }
-// `;
+export const featuredImageFields = `
+  fragment featuredImageFields on FileField {
+    height
+    url
+    width
+    title
+    format
+    size
+    alt
+    responsiveImage {
+      aspectRatio
+      alt
+      base64
+      bgColor
+      height
+      sizes
+      src
+      srcSet
+      title
+      webpSrcSet
+      width
+    }
+  }
+`;
 
 export const seoMetaTagsFields = `
   fragment seoMetaTagsFields on Tag {
@@ -52,19 +74,49 @@ export const siteQuery = `
           title
           description
           image {
-            responsiveImage {
-              ...responsiveImageFields
-            }
-            alt
-            height
-            url
+            ...featuredImageFields
           }
         }
       }
     }
   }
-  ${responsiveImageFields}
+  ${featuredImageFields}
   ${seoMetaTagsFields}
+`;
+
+export const pageQuery = `
+query Page($slug: String!, $locale: SiteLocale!) {
+  page(locale: $locale, filter: {slug: {eq: $slug}}) {
+    slug
+    title
+    id
+    featuredImage {
+      ...featuredImageFields
+    }
+    content {
+      ...contentFieldFragment
+    }
+  }
+}
+${contentFieldFragment}
+${featuredImageFields}
+`;
+
+export const allCategoriesQuery = `
+query AllCategories($locale: SiteLocale!) {
+  allCategories {
+    id
+    title
+    featuredImage {
+      ...featuredImageFields
+    }
+    link {
+      slug(locale: $locale)
+      title(locale: $locale)
+    }
+  }
+}
+${featuredImageFields}
 `;
 
 export const formatDate = (date: string) => format(parseISO(date), 'PPP');
